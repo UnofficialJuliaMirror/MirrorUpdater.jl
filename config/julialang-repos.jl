7a0f3587795178a275c8ec4be5375387557a8d51
Dilum Aluthge <dilum@aluthge.com>
2018-10-07 06:04:46 -0400

    Fix typos

--- config/julialang-repos.jl
+++ config/julialang-repos.jl
@@ -4,13 +4,13 @@ const JULIALANG_REPOS = MirrorUpdater.Types.SrcDestPair[
         source_url = "https://github.com/JuliaLang/METADATA.jl",
         destination_repo_name = "JuliaLang-METADATA.jl",
         ),
-    MirrorUpdater.SrcDestPair(
+    MirrorUpdater.Types.SrcDestPair(
         ;
         source_url = "https://github.com/JuliaLang/Pkg.jl",
         destination_repo_name =
             "JuliaLang-Pkg.jl-44cfe95a-1eb2-52ea-b672-e2afdf69b78f",
         ),
-    MirrorUpdater.SrcDestPair(
+    MirrorUpdater.Types.SrcDestPair(
         ;
         source_url = "https://github.com/JuliaLang/julia",
         destination_repo_name = "JuliaLang-julia",
