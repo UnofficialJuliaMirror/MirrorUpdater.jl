dfbaefc4d4db6b7b64b98830cccea2cd8855ca41
Dilum Aluthge <dilum@aluthge.com>
2018-10-08 01:43:31 -0400

    Progress commit

--- config/repositories/julialang-repos.jl
+++ config/repositories/julialang-repos.jl
@@ -10,11 +10,11 @@ const JULIALANG_REPOS = MirrorUpdater.Types.SrcDestPair[
         destination_repo_name =
             "JuliaLang-Pkg.jl-44cfe95a-1eb2-52ea-b672-e2afdf69b78f",
         ),
-    MirrorUpdater.Types.SrcDestPair(
-        ;
-        source_url = "https://github.com/JuliaLang/julia",
-        destination_repo_name = "JuliaLang-julia",
-        ),
+    # MirrorUpdater.Types.SrcDestPair(
+    #     ;
+    #     source_url = "https://github.com/JuliaLang/julia",
+    #     destination_repo_name = "JuliaLang-julia",
+    #     ),
     MirrorUpdater.Types.SrcDestPair(
         ;
         source_url = "https://github.com/JuliaLang/libosxunwind",
