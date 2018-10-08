eac12ff63bb2d7b1b751cd5ac8bcc5cb302d05d1
Dilum Aluthge <dilum@aluthge.com>
2018-10-07 23:26:41 -0400

    Backup commit

--- src/MirrorUpdater.jl
+++ src/MirrorUpdater.jl
@@ -6,11 +6,17 @@ __precompile__(true)
 
 include(joinpath("Types", "Types.jl"))
 
-include(joinpath("Common", "Common.jl"))
-include(joinpath("Hosts", "Hosts.jl"))
-include(joinpath("Run", "Run.jl"))
 include(joinpath("Utils", "Utils.jl"))
-include(joinpath("GitHubMirrorUpdater", "GitHubMirrorUpdater.jl"))
+
+include(joinpath("Hosts", "Hosts.jl"))
+
+# include(joinpath("GitHubMirrorUpdater", "GitHubMirrorUpdater.jl"))
+
+include(joinpath("Common", "Common.jl"))
+
+include(joinpath("CommandLine", "CommandLine.jl"))
+
+include(joinpath("Run", "Run.jl"))
 
 end # End module MirrorUpdater
 
