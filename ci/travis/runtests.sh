2079e13da9b6cc730f8f344c43f9da4cc081abcf
Dilum Aluthge <dilum@aluthge.com>
2018-10-14 13:24:33 -0400

    Progress commit

--- ci/travis/runtests.sh
+++ ci/travis/runtests.sh
@@ -4,22 +4,27 @@
 
 set -ev
 
-julia --check-bounds=yes --color=yes -e '
-    import Pkg;
-    Pkg.build("MirrorUpdater");
-    '
+export JULIA_FLAGS="--code-coverage=all --check-bounds=yes --color=yes"
 
-julia --check-bounds=yes --color=yes -e '
-    import MirrorUpdater;
-    '
+echo "JULIA_FLAGS=$JULIA_FLAGS"
 
-julia --check-bounds=yes --color=yes -e '
-    import Pkg;
-    Pkg.test("MirrorUpdater"; coverage=true);
-    '
+julia $JULIA_FLAGS -e 'import Pkg; Pkg.build("MirrorUpdater");'
+
+julia $JULIA_FLAGS -e 'import MirrorUpdater;'
+
+julia $JULIA_FLAGS -e 'import Pkg; Pkg.test("MirrorUpdater"; coverage=true);'
 
 cat Project.toml
 
 cat Manifest.toml
+
+# julia $JULIA_FLAGS -e 'import Pkg; try Pkg.add("Coverage") catch end;'
+
+# julia $JULIA_FLAGS -e '
+#     import Coverage;
+#     import MirrorUpdater;
+#     cd(MirrorUpdater.package_directory());
+#     Coverage.Codecov.submit(Coverage.Codecov.process_folder());
+#     '
 
 ##### End of file
