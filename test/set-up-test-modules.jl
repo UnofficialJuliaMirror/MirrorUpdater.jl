2195a3f9206c0471a10d31ac20e21ddebb987461
Dilum Aluthge <dilum@aluthge.com>
2018-10-14 13:47:37 -0400

    Progress commit

--- test/set-up-test-modules.jl
+++ test/set-up-test-modules.jl
@@ -1,11 +1,11 @@
 ##### Beginning of file
 
-testmodulea_filename::String = joinpath("TestModuleA", "TestModuleA.jl")
-testmoduleb_filename::String  = joinpath(
+testmodulea_filename = joinpath("TestModuleA", "TestModuleA.jl")
+testmoduleb_filename  = joinpath(
         "TestModuleB", "directory1", "directory2", "directory3",
         "directory4", "directory5", "TestModuleB.jl",
         )
-testmodulec_filename::String = joinpath(mktempdir(), "TestModuleC.jl")
+testmodulec_filename = joinpath(mktempdir(), "TestModuleC.jl")
 rm(testmodulec_filename; force = true, recursive = true)
 open(testmodulec_filename, "w") do io
     write(io, "module TestModuleC end")
