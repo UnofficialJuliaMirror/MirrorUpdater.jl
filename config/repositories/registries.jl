5aa1028d1bddc1ef24e3c4a5e63d3f3db33b1f86
Dilum Aluthge <dilum@aluthge.com>
2019-02-10 14:24:37 -0500

    Add HolyLab/HolyLabRegistry to list of registries

--- config/repositories/registries.jl
+++ config/repositories/registries.jl
@@ -13,4 +13,11 @@ const REGISTRY_LIST = MirrorUpdater.Types.Registry[
         uuid = "ae0cb698-197b-42ec-a0a0-4f871aea6013",
         url = "https://github.com/fredrikekre/Registry",
         ),
+    MirrorUpdater.Types.Registry(
+        ;
+        owner = "HolyLab",
+        name = "HolyLabRegistry",
+        uuid = "c72e0888-c1c3-11e8-0741-c1bd6f46e678",
+        url = "https://github.com/HolyLab/HolyLabRegistry",
+        ),
     ]
