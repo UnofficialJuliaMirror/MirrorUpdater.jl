7b830b3ea29762dd7a6c8cb33749458a5ad990d4
Dilum Aluthge <dilum@aluthge.com>
2019-03-04 22:07:17 -0500

    Add bcbi/PredictMDRegistry

--- config/repositories/registries.jl
+++ config/repositories/registries.jl
@@ -13,4 +13,11 @@ const REGISTRY_LIST = MirrorUpdater.Types.Registry[
         uuid = "ae0cb698-197b-42ec-a0a0-4f871aea6013",
         url = "https://github.com/fredrikekre/Registry",
         ),
+    MirrorUpdater.Types.Registry(
+        ;
+        owner = "bcbi",
+        name = "PredictMDRegistry",
+        uuid = "26a550a3-39fe-4af4-af6d-e8814c2b6dd9",
+        url = "https://github.com/bcbi/PredictMDRegistry",
+        ),
     ]
