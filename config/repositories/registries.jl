0cc3d69defc98ad428f19e64e0ffdb7c94cd004a
Dilum Aluthge <dilum@aluthge.com>
2019-03-13 12:01:16 -0400

    Add JuliaFinance registry

--- config/repositories/registries.jl
+++ config/repositories/registries.jl
@@ -1,23 +1,30 @@
 const REGISTRY_LIST = MirrorUpdater.Types.Registry[
     MirrorUpdater.Types.Registry(
         ;
+        owner = "bcbi",
+        name = "PredictMDRegistry",
+        uuid = "26a550a3-39fe-4af4-af6d-e8814c2b6dd9",
+        url = "https://github.com/bcbi/PredictMDRegistry.git",
+        ),
+    MirrorUpdater.Types.Registry(
+        ;
         owner = "JuliaRegistries",
         name = "General",
         uuid = "23338594-aafe-5451-b93e-139f81909106",
-        url = "https://github.com/JuliaRegistries/General",
+        url = "https://github.com/JuliaRegistries/General.git",
         ),
     MirrorUpdater.Types.Registry(
         ;
         owner = "fredrikekre",
         name = "Registry",
         uuid = "ae0cb698-197b-42ec-a0a0-4f871aea6013",
-        url = "https://github.com/fredrikekre/Registry",
+        url = "https://github.com/fredrikekre/Registry.git",
         ),
     MirrorUpdater.Types.Registry(
         ;
-        owner = "bcbi",
-        name = "PredictMDRegistry",
-        uuid = "26a550a3-39fe-4af4-af6d-e8814c2b6dd9",
-        url = "https://github.com/bcbi/PredictMDRegistry",
+        owner = "JuliaFinance",
+        name = "JuliaFinance",
+        uuid = "e8ee1221-7013-525c-b886-deb0c05ff931",
+        url = "https://github.com/JuliaFinance/JuliaFinance.git",
         ),
     ]
