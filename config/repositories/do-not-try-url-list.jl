18ab4429ec613f3b4416a24bce595a3d2ce914d7
Dilum Aluthge <dilum@aluthge.com>
2019-04-30 02:11:00 -0400

    Temporarliy add Pandas to the do-not-try list

--- config/repositories/do-not-try-url-list.jl
+++ config/repositories/do-not-try-url-list.jl
@@ -37,4 +37,5 @@ const DO_NOT_TRY_URL_LIST = String[
     "https://github.com/rennis250/SDL.jl.git",
     "https://github.com/rennis250/Sparrow.jl.git",
     "https://github.com/sadit/SimilarReferences.jl.git",
+    "https://github.com/JuliaPy/Pandas.jl",
     ]
