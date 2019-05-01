63e0c79f4186b85b2d50ed063c23ea9ec20ccf10
Dilum Aluthge <dilum@aluthge.com>
2019-05-01 15:56:54 -0400

    Update git-lfs-repos-url-list.jl

--- config/repositories/git-lfs-repos-url-list.jl
+++ config/repositories/git-lfs-repos-url-list.jl
@@ -2,6 +2,12 @@ import Pkg
 
 const GIT_LFS_REPO_URL_LIST = String[
     x["source_url"] for x in values(
-        Pkg.TOML.parsefile("git-lfs-repos-src-dest-pairs.toml")
+        Pkg.TOML.parsefile(
+            joinpath(
+                @__DIR__,
+                "git-lfs-repos-src-dest-pairs.toml",
+                )
+            )
         )
     ]
+
