efeafccb17f2ad7ef015f8a8d7485a7d90bf2df0
Dilum Aluthge <dilum@aluthge.com>
2019-05-02 00:57:58 -0400

    Update unregistered-packages.jl

--- config/repositories/unregistered-packages.jl
+++ config/repositories/unregistered-packages.jl
@@ -23,27 +23,6 @@ const UNREGISTERED_PACKAGES = MirrorUpdater.Types.SrcDestPair[
     MirrorUpdater.Types.SrcDestPair(
         ;
         source_url =
-            "https://github.com/alan-turing-institute/MLJ.jl",
-        destination_repo_name =
-            "MLJ.jl-add582a8-e3ab-11e8-2d5e-e98b27df1bc7",
-        ),
-    MirrorUpdater.Types.SrcDestPair(
-        ;
-        source_url =
-            "https://github.com/alan-turing-institute/MLJBase.jl",
-        destination_repo_name =
-            "MLJBase.jl-a7f614a8-145f-11e9-1d2a-a57a1082229d",
-        ),
-    MirrorUpdater.Types.SrcDestPair(
-        ;
-        source_url =
-            "https://github.com/alan-turing-institute/MLJModels.jl",
-        destination_repo_name =
-            "MLJModels.jl-d491faf4-2d78-11e9-2867-c94bc002c0b7",
-        ),
-    MirrorUpdater.Types.SrcDestPair(
-        ;
-        source_url =
             "https://github.com/alan-turing-institute/MLJRegistry.jl",
         destination_repo_name =
             "MLJRegistry.jl-a41df0fa-2d8a-11e9-1bfb-8110be68cd3e",
