27fdc4404fada2c2015dc5dd5fd4c7fd22606d02
Dilum Aluthge <dilum@aluthge.com>
2019-05-03 23:02:32 -0400

    Add DelayedErrors.jl

--- config/repositories/unregistered-packages.jl
+++ config/repositories/unregistered-packages.jl
@@ -2,23 +2,16 @@ const UNREGISTERED_PACKAGES = MirrorUpdater.Types.SrcDestPair[
     MirrorUpdater.Types.SrcDestPair(
         ;
         source_url =
-            "https://github.com/UnofficialJuliaMirror/MirrorUpdater.jl",
+            "https://github.com/DilumAluthge/DelayedErrors.jl",
         destination_repo_name =
-            "MirrorUpdater.jl-7f0fc4bf-4ffe-4e21-ab91-7bf1358d5ab3",
+            "DelayedErrors.jl-b0cb2517-35d6-4513-a0ca-b0b388a189e4",
         ),
     MirrorUpdater.Types.SrcDestPair(
         ;
         source_url =
-            "https://github.com/UnofficialJuliaMirrorSnapshots/Snapshots.jl",
+            "https://github.com/JuliaMath/Libm.jl",
         destination_repo_name =
-            "Snapshots.jl-44eb87bc-f37b-45e8-9f53-3bcb453a652d",
-        ),
-    MirrorUpdater.Types.SrcDestPair(
-        ;
-        source_url =
-            "https://github.com/UnofficialJuliaMirrorSnapshots/RemoveLFS.jl",
-        destination_repo_name =
-            "RemoveLFS.jl-a262596c-dd27-4d54-b019-3619024addbd",
+            "JuliaMath-Libm.jl",
         ),
     MirrorUpdater.Types.SrcDestPair(
         ;
@@ -30,8 +23,22 @@ const UNREGISTERED_PACKAGES = MirrorUpdater.Types.SrcDestPair[
     MirrorUpdater.Types.SrcDestPair(
         ;
         source_url =
-            "https://github.com/JuliaMath/Libm.jl",
+            "https://github.com/UnofficialJuliaMirror/MirrorUpdater.jl",
         destination_repo_name =
-            "JuliaMath-Libm.jl",
+            "MirrorUpdater.jl-7f0fc4bf-4ffe-4e21-ab91-7bf1358d5ab3",
+        ),
+    MirrorUpdater.Types.SrcDestPair(
+        ;
+        source_url =
+            "https://github.com/UnofficialJuliaMirrorSnapshots/RemoveLFS.jl",
+        destination_repo_name =
+            "RemoveLFS.jl-a262596c-dd27-4d54-b019-3619024addbd",
+        ),
+    MirrorUpdater.Types.SrcDestPair(
+        ;
+        source_url =
+            "https://github.com/UnofficialJuliaMirrorSnapshots/Snapshots.jl",
+        destination_repo_name =
+            "Snapshots.jl-44eb87bc-f37b-45e8-9f53-3bcb453a652d",
         ),
     ]
